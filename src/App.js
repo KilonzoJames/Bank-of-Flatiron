56a5ec6cde8a76b50502c7dd2caa499a92caf44f
KilonzoJames <jay.kilonzo@gmail.com>
2023-07-23 22:04:37 +0300

    Component Abstration

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import Search from "./Search";
 import Form from "./Form";
+import TableList from "./TableList";
 import "./App.css";
 
 
 function App() {
+  const [array, setArray]=useState([]);
+
+  const updateArray = (newArray) => {
+    setArray(newArray);
+  };
+    // Callback function to update the state from the Form component
+
+
   return (
     <div className="App">
      <Header/>
-     <Form/>
+     <Search 
+     array={array} 
+     setArray={setArray}
+     />
+     <Form 
+     array={array} 
+     updateArray={updateArray}
+     />
+     <TableList 
+     array={array} 
+     setArray={setArray}
+     />
     </div>
   );
 }
