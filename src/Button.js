dc1a5da9553515ec8e96bd272495dc363a95ec06
KilonzoJames <jay.kilonzo@gmail.com>
2023-07-24 15:32:34 +0300

    url

--- src/Button.js
+++ src/Button.js
@@ -2,11 +2,12 @@ import React from "react";
 
 function Button({children, onClick}){
     return(
-                <button 
-                onClick={onClick}  
-                className="btn btn-outline-primary">
-                    {children}
-                </button>
+    <button 
+    onClick={onClick}  
+    className="btn btn-outline-primary"
+    >
+    {children}
+    </button>
     );
 }
 export default Button
